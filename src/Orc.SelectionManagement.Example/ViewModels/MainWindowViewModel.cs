30a234e0bfd5a120ec9d1d8913b62de41dcbb84f
Geert van Horrik <geert@catenalogic.com>
2026-01-06 20:04:11 +0100

    Update example project to Catel 7

--- src/Orc.SelectionManagement.Example/ViewModels/MainWindowViewModel.cs
+++ src/Orc.SelectionManagement.Example/ViewModels/MainWindowViewModel.cs
@@ -4,23 +4,18 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
-using Catel;
 using Catel.Collections;
-using Catel.Logging;
 using Catel.MVVM;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private static readonly ILog Log = LogManager.GetCurrentClassLogger();
-
     private readonly ISelectionManager<int> _intSelectionManager;
     private readonly ISelectionManager<string> _stringSelectionManager;
 
-    public MainWindowViewModel(ISelectionManager<int> intSelectionManager, ISelectionManager<string> stringSelectionManager)
+    public MainWindowViewModel(IServiceProvider serviceProvider, 
+        ISelectionManager<int> intSelectionManager, ISelectionManager<string> stringSelectionManager)
+        : base(serviceProvider)
     {
-        ArgumentNullException.ThrowIfNull(intSelectionManager);
-        ArgumentNullException.ThrowIfNull(stringSelectionManager);
-
         _intSelectionManager = intSelectionManager;
         _stringSelectionManager = stringSelectionManager;
 
